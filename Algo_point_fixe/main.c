2630c4a669ead162a8a349d509d21162c2d40869
Valentin Pawlak <valentin@pwlk.fr>
2025-09-15 22:22:46 +0200

    started point fixe algorithm got heap buffer overflow error on v2d_get_line

---
+++ Algo_point_fixe/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "vector.h"
+
+
+void mul_line(Vector* line, int n) {
+    for (int i = 0; i < line->lenght; i++) {
+        v_set(line, i, *v_get(line, i) * n);
+    }
+}
+
+Vector* add_lines(Vector* l1, Vector* l2) {
+    Vector* new_line = InitVector();
+
+    if (l1->lenght != l2->lenght) { return new_line; }
+    
+    for (int i = 0; i < l1->lenght; i++) {
+        v_append(new_line, *v_get(l1, i) + *v_get(l2, i));
+    }
+
+    return new_line;
+}
+
+
+void point_fixe(Vector2d* input) {
+    Vector* pivot = v2d_get_line(input, 0);
+    Vector* current_line;
+    Vector* result_line;
+    int x_pivot;
+    int x_used;
+
+    for (int y = 1; y < input->shape[1]; y++) {
+        current_line = v2d_get_line(input, y);
+        printf("%d | %d\n", y, current_line->lenght);
+
+        x_pivot = *v_get(pivot, 0);
+        x_used = *v_get(current_line, 0);
+
+        mul_line(pivot, x_used);
+        mul_line(current_line, -x_pivot);
+
+        result_line = add_lines(current_line, pivot);
+
+        v2d_set_line(input, y, result_line->array, result_line->lenght);
+        
+        DestroyVector(current_line);
+        DestroyVector(result_line);
+    }
+
+    DestroyVector(pivot);
+}
+
+
+int main() {
+    Vector2d* input = Init2dVector(4, 3, 0);
+
+    v2d_set_line(input, 0, (int[]) {5, 9, 2, 3}, 4);
+    v2d_set_line(input, 1, (int[]) {8, 4, 6, 1}, 4);
+    v2d_set_line(input, 2, (int[]) {5, 8, 6, 2}, 4);
+
+    v2d_print(input);
+
+    point_fixe(input);
+    printf("\n");
+
+    v2d_print(input);
+
+    Destroy2dVector(input);
+    return 0;
+}
+
+
--- includes/vector.h
+++ includes/vector.h
@@ -201,4 +201,182 @@ int v_count(Vector* v, int value) {
     return count;
 }
 
+
+struct Vector2d;
+
+typedef struct Vector2d {
+    int* shape;
+    int lenght;
+    int* array;
+} Vector2d;
+
+Vector2d* Init2dVector(int w, int h, int value) {
+    Vector2d* v = malloc(sizeof(Vector2d));
+    if (!v) { return NULL; }
+
+    v->shape = malloc(sizeof(int)*2);
+    v->shape[0] = w;
+    v->shape[1] = h;
+
+    v->lenght = w*h;
+    v->array = malloc(sizeof(int)*v->lenght);
+
+    for (int i = 0; i < v->lenght; i++) { v->array[i] = value; }
+
+    return v;
+}
+
+Vector2d* Copy2dVector(Vector2d* v) {
+    Vector2d* new_v = malloc(sizeof(Vector));
+    if (!v) { return NULL; }
+    new_v->lenght = v->lenght;
+    new_v->shape = v->shape;
+    new_v->array = malloc(sizeof(int)*new_v->lenght);
+
+    for (int i = 0; i < v->lenght; i++) { new_v->array[i] = v->array[i]; }
+
+    return new_v;
+}
+
+void Destroy2dVector(Vector2d* v) {
+    free(v->shape);
+    free(v->array);
+    free(v);
+}
+
+int* v2d_get(Vector2d* v, int x, int y) {
+    if (x < 0 || x >= v->shape[0]) { return NULL; }
+    if (y < 0 || y >= v->shape[1]) { return NULL; }
+
+    return &v->array[y*v->shape[0] + x];
+}
+
+void v2d_set(Vector2d* v, int x, int y, int value) {
+    if (x < 0 || x >= v->shape[0]) { return; }
+    if (y < 0 || y >= v->shape[1]) { return; }
+
+    v->array[y*v->shape[0] + x] = value;
+}
+
+void v2d_set_line(Vector2d* v, int y, int* line, int line_size) {
+    if (y < 0 || y >= v->shape[1]) { return; }
+    if (line_size > v->shape[0]) { return; }
+
+    int i = y * v->shape[0];
+
+    for (int i0 = 0; i0 < line_size; i0++) {
+        v->array[i] = line[i0];
+        i++;
+    }
+}
+
+Vector* v2d_get_line(Vector2d* v, int y) {
+    Vector* line = InitVector();
+
+    // if (y < 0 || y >= v->shape[1]) { return line; }
+
+    for (int i = y * v->shape[0]; i < v->shape[0]; i++) {
+        printf("adding %d at index %d to the new vector\n", v->array[i], i);
+        v_append(line, v->array[i]);
+    }
+
+    v_print(line);
+    return line;
+}
+
+void v2d_print(Vector2d* v) {
+    printf("[");
+    for (int y = 0; y < v->shape[1]; y++) {
+        if (y != 0) { printf(" "); }
+        printf("[ ");
+
+        for (int x = 0; x < v->shape[0]; x++) {
+            printf("%d ", v->array[y*v->shape[0] + x]);
+        }
+
+        printf("]");
+        if (y == v->shape[1] - 1) { break; }
+        printf("\n");
+    }
+    printf("]\n");
+}
+
+
+int v2d_min(Vector2d* v) {
+    if (v->lenght == 0) { return -32768; }
+    int min = v->array[0];
+
+    for (int i = 1; i < v->lenght; i++) {
+        if (v->array[i] < min) {
+            min = v->array[i];
+        }
+    }
+
+    return min;
+}
+
+int v2d_max(Vector2d* v) {
+    if (v->lenght == 0) { return 32768; }
+    int max = v->array[0];
+
+    for (int i = 1; i < v->lenght; i++) {
+        if (v->array[i] > max) {
+            max = v->array[i];
+        }
+    }
+
+    return max;
+}
+
+void v2d_sort(Vector2d* v) {
+    int i, j;
+    int swapped;
+
+    for (i = 0; i < v->lenght - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < v->lenght - i - 1; j++) {
+            if (v->array[j] > v->array[j + 1]) {
+                swap(&(v->array[j]), &(v->array[j + 1]));
+                swapped = 1;
+            }
+        }
+
+        if (swapped == 0) {
+            break;
+        }
+    }
+}
+
+int* v2d_coords(Vector2d* v, int value) {
+    int* output = malloc(sizeof(int) * 2);
+    output[0] = -1;
+    output[1] = -1;
+    
+    if (v->lenght == 0) { return output; }
+
+    for (int x = 0; x < v->shape[0]; x++) {
+        for (int y = 0; y < v->shape[1]; y++) {
+
+            if (v->array[y*v->shape[0] + x] == value) {
+                output[0] = x;
+                output[1] = y;
+                return output;
+            }
+            
+        }
+    }
+    
+    return output;
+}
+
+int v2d_count(Vector2d* v, int value) {
+    int count = 0;
+
+    for (int i = 0; i < v->lenght; i++) {
+        if (v->array[i] == value) { count++; }
+    }
+
+    return count;
+}
+
 #endif
